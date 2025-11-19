f8febe8976232875f98cce7014970e3036e991b8
새아가는아가야 <93969942+DobbyIsAdobby@users.noreply.github.com>
2025-11-19 18:59:35 +0900

    Update index.py

--- index.py
+++ index.py
@@ -169,6 +169,14 @@ if uploaded_file:
 
 
 
+    #title
+st.title('Task5: 파일 업로드')
+
+uploaded_file = st.file_uploader("Upload Your data", type="csv")
+
+if uploaded_file is not None:
+    df = pd.read_csv(uploaded_file)
+    
     st.write("Uploaded Data")
     st.write(df)
 
